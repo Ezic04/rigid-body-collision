c4c81ab28c8131f7dbccff9f73a715854ef135b6
Ezic04 <slawek.tomasz.brzozka@gmail.com>
2026-07-16 22:36:04 +0200

    flying square

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
+use macroquad::math;
 use macroquad::prelude::*;
+use std::f32::consts;
 
-#[macroquad::main("MyGame")]
+struct Square {
+    center: Vec2,
+    rot: f32,
+    r: f32,
+}
+
+struct State {
+    sq: Square,
+    t: f32,
+}
+
+impl State {
+    fn update(&mut self) {
+        let dt = 0.001;
+        let two_pi = 2.0 * consts::PI;
+        self.t += dt;
+        if self.t > two_pi {
+            self.t -= two_pi
+        }
+        self.sq.center.x += 10.0 * self.t.sin();
+    }
+}
+
+struct Scene {
+    scale: f32,
+}
+
+impl Default for Scene {
+    fn default() -> Self {
+        Scene { scale: 0.01 }
+    }
+}
+
+#[macroquad::main("Colisions")]
 async fn main() {
+    let mut t = 0.0;
+    let dt = 0.001;
     loop {
-        clear_background(RED);
-
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        let two_pi = 2.0 * consts::PI;
+        t += dt;
+        if t > two_pi {
+            t -= two_pi
+        }
+        let h = screen_height();
+        let w = screen_width();
+        clear_background(WHITE);
+        draw_poly(w / 2.0 + w / 4.0 * t.sin(), h / 2.0, 4, 128.0, 0.0, BLACK);
         next_frame().await
     }
 }
