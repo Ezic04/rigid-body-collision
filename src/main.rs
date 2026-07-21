48d5185a0425f5f6ea25e37addcd4ec55d25861e
Ezic04 <slawek.tomasz.brzozka@gmail.com>
2026-07-21 21:56:25 +0200

    implement coordinate system and angular velocity

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,95 @@
-use macroquad::math;
 use macroquad::prelude::*;
 use std::f32::consts;
 
+#[derive(Debug)]
 struct Square {
-    center: Vec2,
-    rot: f32,
-    r: f32,
+    position: Vec2,
+    linear_velocity: Vec2,
+    rotation: f32,
+    angular_velocity: f32,
+    radius: f32,
+    mass: f32,
 }
 
+impl Square {
+    fn draw(&self, to_window_cords: Affine2) {
+        let window_center = to_window_cords.transform_point2(self.position);
+        let window_radius = to_window_cords
+            .transform_vector2(Vec2::new(self.radius, 0.0))
+            .x;
+        draw_poly(
+            window_center.x,
+            window_center.y,
+            4,
+            window_radius,
+            f32::to_degrees(self.rotation),
+            BLACK,
+        );
+    }
+}
+
+impl Default for Square {
+    fn default() -> Self {
+        let r = 0.5;
+        let density = 10.0;
+        Self {
+            position: Vec2::new(2.0, 2.0),
+            linear_velocity: Vec2::new(1.0, 0.5),
+            rotation: 0.0,
+            angular_velocity: 0.1 * consts::TAU,
+            radius: r,
+            mass: density * r,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct State {
-    sq: Square,
-    t: f32,
+    square: Square,
+    time: f32,
 }
 
 impl State {
     fn update(&mut self) {
-        let dt = 0.001;
-        let two_pi = 2.0 * consts::PI;
-        self.t += dt;
-        if self.t > two_pi {
-            self.t -= two_pi
+        let dt = get_frame_time();
+        self.time += dt;
+        if self.time > consts::TAU {
+            self.time -= consts::TAU
         }
-        self.sq.center.x += 10.0 * self.t.sin();
+        let sq = &mut self.square;
+        sq.position += sq.linear_velocity * dt;
+        sq.rotation += sq.angular_velocity * dt;
+    }
+
+    fn draw(&self, to_window_cords: Affine2) {
+        clear_background(WHITE);
+        self.square.draw(to_window_cords)
     }
 }
 
-struct Scene {
-    scale: f32,
-}
-
-impl Default for Scene {
+impl Default for State {
     fn default() -> Self {
-        Scene { scale: 0.01 }
+        Self {
+            square: Square::default(),
+            time: 0.0,
+        }
     }
+}
+
+fn window_coords_transform(window_height: f32) -> Affine2 {
+    let scale = 100.0;
+    Affine2::from_translation(Vec2::new(0.0, window_height))
+        * Affine2::from_scale(scale * Vec2::new(1.0, -1.0))
 }
 
 #[macroquad::main("Colisions")]
 async fn main() {
-    let mut t = 0.0;
-    let dt = 0.001;
+    let mut state = State::default();
     loop {
-        let two_pi = 2.0 * consts::PI;
-        t += dt;
-        if t > two_pi {
-            t -= two_pi
-        }
-        let h = screen_height();
-        let w = screen_width();
-        clear_background(WHITE);
-        draw_poly(w / 2.0 + w / 4.0 * t.sin(), h / 2.0, 4, 128.0, 0.0, BLACK);
+        let height = screen_height();
+        let to_window_cords = window_coords_transform(height);
+        state.update();
+        state.draw(to_window_cords);
         next_frame().await
     }
 }
